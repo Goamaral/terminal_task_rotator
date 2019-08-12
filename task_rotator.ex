9708aec939bae126e0b8277f9864c5eee30fa0ff
Goncalo Amaral <goa.docs@gmail.com>
2019-08-13 00:25:53 +0100

    Improved score method, next task display and available tasks listing

--- task_rotator.ex
+++ task_rotator.ex
@@ -1,3 +1,8 @@
+### TODO ###
+# Read projects from file
+# Save projects to file on exit
+# Split in different files
+
 defmodule Terminal do
   def bold(text, print \\ true) do
     str = "#{IO.ANSI.bright}#{text}#{IO.ANSI.reset}"
@@ -112,16 +117,64 @@ defmodule TaskMenu do
       "G" -> next(state_pid); Router.go_to(:tasks, state_pid)
       "L" -> list(state_pid); Router.go_to(:tasks, state_pid)
       "B" -> Router.go_to(:home, state_pid)
+      _ -> index(state_pid)
     end
   end
 
-  def next(state_id) do
-    project = State.next(state_pid)
-    # TODO: Display task
+  def next(state_pid) do
+    { project, id } = State.next(state_pid)
+    Terminal.clear
+    Terminal.bold "Next task"
+
+    unless project == nil do
+      IO.puts "#{project.name} Due date: #{project.due_date} Priority: #{project.priority}"
+      IO.puts "[C] Mark as completed"
+      IO.puts "[B] Back"
+
+      op = Terminal.ask_option
+
+      case op do
+        "C" -> completed(state_pid, id, project)
+        _ -> :ok
+      end
+    else
+      Terminal.error "No tasks available"
+      IO.puts "[B] Back"
+
+      Terminal.ask_option
+    end
   end
 
-  def list(state_id) do
-    # TODO: Display uncompleted projects sorted by score
+  def list(state_pid) do
+    Terminal.bold "Tasks"
+
+    projects = State.all(state_pid)
+      |> Enum.reject(fn project -> project.complete end)
+      |> Enum.sort(&(Project.score(&1) > Project.score(&2)))
+
+    size = Enum.count(projects)
+
+    if size > 0 do
+      list_item 0, projects, size
+    else
+      Terminal.error "No tasks available"
+    end
+
+    IO.puts "[B] Back"
+    Terminal.ask_option
+  end
+
+  def list_item(id, projects, size) do
+    if id < size do
+      project = projects |> Enum.at(id)
+      IO.puts "#{project.name} Due date: #{project.due_date} Priority: #{project.priority}"
+      list_item id + 1, projects, size
+    end
+  end
+
+  def completed(state_pid, id, project) do
+    project = %{ project | complete: true }
+    State.update state_pid, id, project
   end
 end
 
@@ -352,14 +405,17 @@ defmodule Project do
   end
 
   def score(project) do
-    days = Date.diff(Date.utc_today, project.due_date) + 1
-    date_comparation = Date.compare(project.due_date, Date.utc_today)
+    days = Date.diff(project.due_date, Date.utc_today)
     priority = project.priority + 1
 
-    if date_comparation == :lt do
-      days * priority
-    else
-      priority / days
+    if days < 0 do
+      -1 * days * priority
+    else 
+      if days > 0 do
+        priority / days
+      else
+        priority
+      end
     end
   end
 end
@@ -402,26 +458,28 @@ defmodule State do
   end
 
   def handle_call(:next, _, state) do
-    next = Enum.reduce([nil | state], fn project, acc ->
-      if acc != nil do
+    { next_project, id } = state
+      |> Enum.with_index
+      |> Enum.reduce({ nil, nil }, fn { project, index }, { acc_project, acc_id } ->
         if project.complete do
-          acc
+          { acc_project, acc_id }
         else
-          acc_score = Project.score(acc)
-          project_score = Project.score(project)
+          if acc_project != nil do
+            acc_score = Project.score(acc_project)
+            project_score = Project.score(project)
 
-          if project_score > acc_score do
-            project
+            if project_score > acc_score do
+              { project, index }
+            else
+              { acc_project, acc_id }
+            end
           else
-            acc
+            { project, index }
           end
         end
-      else
-        project
-      end
-    end)
+      end)
 
-    { :reply, next, state }
+    { :reply, { next_project, id }, state }
   end
 
   # API
